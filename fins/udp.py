04b4fbbaccf29c86c963a4d50c8ecff7f167a7bc
Joe Ryan <jr@aphyt.com>
2023-11-09 17:14:15 -0800

    Remove error suppression

--- fins/udp.py
+++ fins/udp.py
@@ -6,18 +6,20 @@ __email__ = "jr@aphyt.com"
 import socket
 from fins import FinsConnection
 
+
 class UDPFinsConnection(FinsConnection):
     """
 
     """
+
     def __init__(self):
         super().__init__()
-        self.BUFFER_SIZE=4096
-        self.fins_socket=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        self.ip_address='192.168.250.1'
-        self.fins_port=None
+        self.BUFFER_SIZE = 4096
+        self.fins_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.ip_address = '192.168.250.1'
+        self.fins_port = None
 
-    def execute_fins_command_frame(self,fins_command_frame):
+    def execute_fins_command_frame(self, fins_command_frame):
         """Sends FINS command using this connection
 
         Implements the abstract method required of FinsConnection
@@ -26,10 +28,7 @@ class UDPFinsConnection(FinsConnection):
         """
         response = ""
         self.fins_socket.sendto(fins_command_frame, (self.ip_address, self.fins_port))
-        try:
-            response=self.fins_socket.recv(self.BUFFER_SIZE)
-        except Exception as err:
-            print(err)
+        response, address = self.fins_socket.recvfrom(self.BUFFER_SIZE)
         return response
 
     def connect(self, IP_Address, Port=9600, Bind_Port=9600):
@@ -38,9 +37,9 @@ class UDPFinsConnection(FinsConnection):
         :param IP_Address: The IP address of the device you are connecting to
         :param Port: The port that the device and host should listen on (default 9600)
         """
-        self.fins_port=Port
-        self.ip_address=IP_Address
-        self.fins_socket.bind(('',Bind_Port))
+        self.fins_port = Port
+        self.ip_address = IP_Address
+        self.fins_socket.bind(('', Bind_Port))
         self.fins_socket.settimeout(1.0)
 
     def __del__(self):
